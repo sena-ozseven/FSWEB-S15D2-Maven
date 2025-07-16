be67baa5cf7133e9869a4dd73a61a727d4aa8fca
sena <gulozseven@gmail.com>
2025-07-16 16:40:00 +0300

    getTasks()

--- src/main/java/org/example/entity/Task.java
+++ src/main/java/org/example/entity/Task.java
@@ -1,13 +1,61 @@
 package org.example.entity;
 
+import java.util.Objects;
+
 public class Task implements Comparable<Task>{
     private String project;
     private String description;
     private String assignee;
+    private Status status;
+    private Priority priority;
 
+    public Task(String project, String description, String assignee, Status status, Priority priority) {
+        this.project = project;
+        this.description = description;
+        this.assignee = assignee;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+    public Status getStatus() {
+        return status;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
 
     @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(getProject(), task.getProject()) && Objects.equals(getDescription(), task.getDescription()) && Objects.equals(getAssignee(), task.getAssignee()) && status == task.status && priority == task.priority;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getProject(), getDescription(), getAssignee(), status, priority);
+    }
+
+
+    //***
+    @Override
     public int compareTo(Task task) {
-        return 0;
+        int descriptionComparison = this.description.compareTo(task.description);
+        if (descriptionComparison != 0) {
+            return descriptionComparison;
+        }
+        return this.project.compareTo(task.project);
     }
 }
--- src/main/java/org/example/entity/TaskData.java
+++ src/main/java/org/example/entity/TaskData.java
@@ -1,4 +1,38 @@
 package org.example.entity;
 
+import java.util.Set;
+
 public class TaskData {
+    Set<Task> annsTasks;
+    Set<Task> bobsTasks;
+    Set<Task> carolsTasks;
+    Set<Task> unassignedTasks;
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTasks, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    public Set<Task> getAnnsTasks() {
+        return annsTasks;
+    }
+
+    public Set<Task> getBobsTasks() {
+        return bobsTasks;
+    }
+
+    public Set<Task> getCarolsTasks() {
+        return carolsTasks;
+    }
+
+    public Set<Task> getUnassignedTasks() {
+        return unassignedTasks;
+    }
+    
+    /*
+    public Set<Task> getTasks(String name) {
+        Set<String> name = new Set<Task>();
+    }*/
 }
